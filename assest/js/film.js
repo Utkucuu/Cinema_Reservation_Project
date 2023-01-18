65a00ab8bb6a55fe6004ebbf5952a17314a44267
utku <bayraktarogluutku@gmail.com>
2023-01-18 14:23:32 +0300

    filmObject start

---
+++ assest/js/film.js
@@ -0,0 +1,11 @@
+function FilmObject(name, genre, director, actors, image, fragmanUrl, description) {  
+
+    this.name = name
+    this.genre = genre
+    this.director = director
+    this.actors = actors
+    this.image = image
+    this.fragmanUrl = fragmanUrl
+    this.description = description
+
+}
---
+++ assest/js/ui.js
@@ -0,0 +1,3 @@
+const film = new FilmObject("Lorenzo’nun Yağı", "Dram", "GeorgeMiller", ["Nick Nolte", "Susan Sarandon", "Peter Ustinov"],`<img src="/assest/images/lorenzo.jpg" alt="">`,"https://www.youtube.com/watch?v=DVglxC31t-g","Lorenzo'nun Yağı 1992 yapımı dram filmi olup, George Miller tarafından yönetilmiştir. Yaşanmış bir öyküden alınan film; Augosto Odone ve Michaela Odone adlı anne babanın, amansız bir hastalık olan ALD'ye yakalanan oğulları Lorenzo Odone'yi ölümden kurtarma çabalarını anlatır." )
+
+

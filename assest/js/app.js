cb2369a687219470ca03b628ec463b9f9abdd0a2
utku <bayraktarogluutku@gmail.com>
2023-01-20 20:14:01 +0300

    mobile nav btn active

--- assest/js/app.js
+++ assest/js/app.js
@@ -1,1 +1,23 @@
+const mobMenuIcon = document.querySelectorAll("svg")[0]
+const mobMenuIcon2 = document.querySelectorAll("svg")[1]
 
+//Mobile menu move icon
+let num = 0
+$("#navMobilBtn").click(function () {
+    num++
+    $("#mobile-menu").toggle("slow", function () {
+
+        if (num == 1) {
+            $(mobMenuIcon).hide()
+            $(mobMenuIcon2).show()
+
+        }
+        if (num == 2) {
+            $(mobMenuIcon).show(1)
+            $(mobMenuIcon2).hide()
+            num = 0
+        }
+
+    });
+
+})
